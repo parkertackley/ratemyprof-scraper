05095f31167cd210b02ea68724cbad3b16ab74ae
parkertackley <parkertackley@gmail.com>
2025-02-11 15:04:22 -0500

    feat: get useful data -> into json

--- src/scraper.py
+++ src/scraper.py
@@ -1,33 +1,62 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+import re
 
 def scraper(url):
     # making a GET request
     req = requests.get(url)
+    if req.status_code != 200:
+        raise Exception("Failed to load page")
 
     print(req)
 
     # parsing the html
     soup = BeautifulSoup(req.content, 'html.parser')
 
+    # getting basic information
     professor = soup.find('h1', class_="NameTitle__NameWrapper-dowf0z-2")
+    profNumbs4 = soup.find('a', class_="HeaderRateButton__StyledCompareProfessorButton-rxcxie-1")
     overall = soup.find('div', class_="RatingValue__Numerator-qw8sqy-2")
     wtalod = soup.find_all('div', class_='FeedbackItem__FeedbackNumber-uof32n-1')
+    schoolName = soup.find('a', class_="TeacherTitles__TeacherSchoolLink-new3kl-3")
+    professorNum = -1
+    schoolNum = -1
 
+    # Get id # of the professor
+    if profNumbs4 and profNumbs4.has_attr('href'):
+        href = profNumbs4['href']
+        professorNum = re.findall(r'/compare/professors/(\d+)', href)
+
+    # Get id # of the school
+    if schoolName and schoolName.has_attr('href'):
+        href = schoolName['href']
+        schoolNum = re.findall(r'/school/(\d+)', href)
+
+    # Get would take again and level of difficulty scores
     wtalodList = []
     for entry in wtalod:
         wtalodList.append(entry.text)
 
-    print(professor.text + "\n" +
-          overall.text + "\n" +
-          "Would take again: " + wtalodList[0] + "\n" +
-          "Level of Difficulty: " + wtalodList[1])
+    jsonEntry = {
+        "school-number" : schoolNum,
+        "id-number" : professorNum,
+        "prof-name" : professor.text,
+        "overall" : overall.text,
+        "wta" : wtalodList[0],
+        "lod" : wtalodList[1]
+    }
+
+    print(jsonEntry)
 
 # Alphonce
-scraper('https://www.ratemyprofessors.com/professor/17342')
+# scraper('https://www.ratemyprofessors.com/professor/17342')
 
 # Hartlof
 # scraper('https://www.ratemyprofessors.com/professor/2055417')
 
 # Atri
 # scraper('https://www.ratemyprofessors.com/professor/1434280')
+
+# John Walsh (USC)
+scraper("https://www.ratemyprofessors.com/professor/2422438")
